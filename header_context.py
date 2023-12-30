e257f83b43af743afb6c72caea3e78197952de56
Clément RAOUL <craoul@groupeastek.fr>
2023-12-30 05:46:07 +0100

    update header with MIT license

--- header_context.py
+++ header_context.py
@@ -1,16 +1,35 @@
-# Nom du Projet: Kamas Dashboard
-# Auteur: RAOUL Clément
-# Date de Création: 17-12-2023
-# Description: Ce projet à pour unique but de visualer le cours d'une devise virtuelle
-# Licence: MIT License
+# MIT License
+#
+# Copyright (c) 2023 Clément RAOUL
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 
 import os
 
 directory_path = "."
 
 # Read header from file
-with open("header.txt", "r") as file:
-    header = file.read()
+with open("LICENSE", "r") as file:
+    header = file.readlines()
+    header = "".join(f"# {line}" for line in header)
 
 
 def header_present(file_path: str, header: str) -> bool:
@@ -26,16 +45,12 @@ def header_present(file_path: str, header: str) -> bool:
     """
     with open(file_path, "r") as file:
         content = file.read(len(header))
-    return content == header
+    return header.replace(" ", "") in content.replace(" ", "")
 
 
-def add_header(file_path: str, header: str) -> None:
+def add_header() -> None:
     """
     Add header to file
-
-    Args:
-        file_path (str): file path
-        header (str): header
     """
     for dirpath, _, filenames in os.walk(directory_path):
         for filename in filenames:
@@ -49,4 +64,4 @@ def add_header(file_path: str, header: str) -> None:
                         file.write(header.rstrip("\r\n") + "\n\n" + content)
 
 
-add_header("header.h", header)
+add_header()
