7e1742b436054dfd14dc89cacc93bc377396525a
Clément RAOUL <craoul@groupeastek.fr>
2024-01-08 21:00:05 +0100

    add icons for each buttons

--- src/controllers/graph_line_controller.py
+++ src/controllers/graph_line_controller.py
@@ -71,14 +71,14 @@ def graph_line_controller(value: int):
         [
             dash.html.Div(
                 [
-                    dash.html.P("Prix moyen"),
+                    dash.html.P("Prix moyen", className="title-p"),
                     dash.html.P(metrics[0], className="white-p"),
                 ],
                 className="graph-info",
             ),
             dash.html.Div(
                 [
-                    dash.html.P("Prix minimum"),
+                    dash.html.P("Prix minimum", className="title-p"),
                     dash.html.P(metrics[1], className="white-p"),
                 ],
                 className="graph-info-right",
